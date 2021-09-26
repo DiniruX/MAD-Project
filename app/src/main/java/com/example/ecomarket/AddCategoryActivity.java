d59fa4aa78e01251f30d329fd9986e0420b7a11e
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2021-09-26 15:14:08 +0530

    code changes

--- app/src/main/java/com/example/ecomarket/AddCategoryActivity.java
+++ app/src/main/java/com/example/ecomarket/AddCategoryActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+//add categories here
 public class AddCategoryActivity extends AppCompatActivity {
 
     private TextInputEditText categoryNameEdt,categoryDesEdt,categoryDateEdt,categoryImgEdt;
