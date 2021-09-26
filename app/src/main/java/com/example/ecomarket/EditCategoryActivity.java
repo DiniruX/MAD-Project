97364c12ce5037fe51cf81e2cc49ded3beab29b9
Nethmi Prabodhika <86922122+NethmiPrabodhika@users.noreply.github.com>
2021-09-26 15:35:58 +0530

    Update EditCategoryActivity.java

--- app/src/main/java/com/example/ecomarket/EditCategoryActivity.java
+++ app/src/main/java/com/example/ecomarket/EditCategoryActivity.java
@@ -89,7 +89,7 @@ public class EditCategoryActivity extends AppCompatActivity {
 
             }
         });
-
+//****************************//
         deleteCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
