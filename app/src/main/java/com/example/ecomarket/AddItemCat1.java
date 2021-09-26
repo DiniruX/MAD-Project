796fe48d8227e1d7f7d1a74faec6b734a70fa855
Malshika Peiris <87640784+malshikaPeiris@users.noreply.github.com>
2021-09-26 15:27:40 +0530

    Update AddItemCat1.java

--- app/src/main/java/com/example/ecomarket/AddItemCat1.java
+++ app/src/main/java/com/example/ecomarket/AddItemCat1.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
+//this is add item cat1
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
